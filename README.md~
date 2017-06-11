16c6f3c9acdf9afadd7385022cfceccfd0fe36ef
SHUBHAM GUPTA <SHUBHAM02GUPTA@GMAIL.com>
2017-06-11 21:57:11 +0530

    first commit

---
+++ README.md~
@@ -0,0 +1,8 @@
+A point system in which each country is awarded 4 points for each gold medal,  2 points for each silver medal, and one point for each bronze medal.
+
+Using the numpy.dot function, creating a new dataframe called 'olympic_points_df' that includes:
+
+a) a column called 'country_name' with the country name.
+
+b) a column called 'points' with the total number of points the country earned at the Sochi olympics.
+
---
+++ olympicMedal.py
@@ -0,0 +1,36 @@
+import numpy
+from pandas import DataFrame, Series
+
+
+def numpy_dot():
+   
+
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea',
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+
+    medals = [gold,silver,bronze]
+    df = DataFrame({"country_name":countries,
+                    "gold":gold,
+                    "silver":silver,
+                    "bronze":bronze})
+
+
+    points = numpy.dot(df[['gold','silver','bronze']],[[4],[2],[1]])
+
+
+    df['points'] = df[['gold','silver','bronze']].dot([4, 2, 1])
+    olympic_points_df = df[['country_name','points']]
+
+
+    return olympic_points_df
+
+df =  numpy_dot()
+print(df)
